c738b15b15335fbe7cfd79f52f2145cfe4efb6fd
Shashi-at-242974112771 <shashi25399@gmail.com>
2024-05-31 14:18:05 +0530

    versioning

--- CompanyEmpDemo/Extensions/ServiceExtensions.cs
+++ CompanyEmpDemo/Extensions/ServiceExtensions.cs
@@ -2,6 +2,7 @@
 using LoggerService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
@@ -16,11 +17,11 @@ namespace CompanyEmpDemo.Extensions
             {
                 option.AddPolicy("CoresPolicy", builder =>
                 {
-                     builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader().WithExposedHeaders("X-Pagination");
+                    builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader().WithExposedHeaders("X-Pagination");
                 });
-              
+
             });
 
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
@@ -37,13 +38,13 @@ namespace CompanyEmpDemo.Extensions
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddSingleton<ILoggerManager, LoggerManager>();
 
-        public static void ConfigureRepositoryManager(this IServiceCollection services) => 
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
 
         public static void ConfigureServiceManager(this IServiceCollection services) =>
             services.AddScoped<IServiceManager, ServiceManager>();
 
-        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) => 
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
             builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
 
         public static void AddCustomMediaTypes(this IServiceCollection services)
@@ -51,18 +52,30 @@ namespace CompanyEmpDemo.Extensions
             services.Configure<MvcOptions>(config =>
             {
                 var systemTextJsonOutputFormatter = config.OutputFormatters.OfType<SystemTextJsonOutputFormatter>()?.FirstOrDefault();
-                if(systemTextJsonOutputFormatter != null)
+                if (systemTextJsonOutputFormatter != null)
                 {
                     systemTextJsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.phiapi.hateoas+json");
                     systemTextJsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.phiapi.apiroot+json");
                 }
 
                 var xmlOutputFormatter = config.OutputFormatters.OfType<XmlDataContractSerializerOutputFormatter>()?.FirstOrDefault();
-                if(xmlOutputFormatter != null)
+                if (xmlOutputFormatter != null)
                 {
                     xmlOutputFormatter.SupportedMediaTypes.Add("application/vnd.phiapi.hateoas+xml");
                     xmlOutputFormatter.SupportedMediaTypes.Add("application/vnd.phiapi.apiroot+xml");
                 }
+            });
+        }
+
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+                opt.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                //opt.ApiVersionReader = new QueryStringApiVersionReader("api-version");
             });
         }
     }
---
+++ CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [ApiVersion("2.0", Deprecated = true)]
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesV2Controller : ControllerBase
+    {
+        private readonly IServiceManager service;
+
+        public CompaniesV2Controller(IServiceManager service) => this.service = service;
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await service.CompanyService.GetAllCompaniesAsync(false);
+            var companies2 = companies.Select(com => $"{com.Name} V2");
+            return Ok(companies2);
+        }
+
+    }
+}
